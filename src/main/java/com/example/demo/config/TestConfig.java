e94642fb3c449fe256fa8fdd9a9b9d1055f24cfc
Gabriel Veloso Pinheiro <tr26660es@trf1.gov.br>
2024-11-29 16:10:13 -0300

    Category entity

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.example.demo.config;
 
+import com.example.demo.entities.Category;
 import com.example.demo.entities.Order;
 import com.example.demo.entities.User;
 import com.example.demo.entities.enums.OrderStatus;
+import com.example.demo.repositories.CategoryRepository;
 import com.example.demo.repositories.OrderRepository;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     public void run(String... args) throws Exception {
         User user1 = new User(null, "Gabriel", "gabriel@gmail.com", "999999999","123456");
         User user2 = new User(null, "joão", "joão@gmail.com", "888888888", "654321");
@@ -31,8 +36,13 @@ public class TestConfig implements CommandLineRunner {
         Order order2 = new Order(null, Instant.parse("2024-07-12T20:32:58Z"), OrderStatus.WAITING_PAYMENT, user2);
         Order order3 = new Order(null, Instant.parse("2024-04-10T05:57:32Z"), OrderStatus.WAITING_PAYMENT, user1);
 
+        Category category1 = new Category(null, "Electronics");
+        Category category2 = new Category(null, "Books");
+        Category category3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
+        categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
     }
 
 }
